4f4574aa2cd58b4929a8401d9ae927339dbf3efe
shinyehwan <jghisjl007@naver.com>
2022-11-02 21:55:20 +0900

    테스트를 위한 입출력 리다이렉팅

---
+++ src/test/java/com/ll/exam/TestUtil.java
@@ -0,0 +1,35 @@
+package com.ll.exam;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class TestUtil {
+	// gen == generate 생성하다.
+	public static Scanner genScanner(String input) {
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+
+		return new Scanner(in);
+	}
+
+	public static ByteArrayOutputStream setOutToByteArray() {
+		final ByteArrayOutputStream output = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(output));
+
+		return output;
+	}
+
+	public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		try {
+			output.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
