6f8ec86c0c2d2b598c6db89e58c8ac0490b856e2
shinyehwan <jghisjl007@naver.com>
2022-11-02 21:47:34 +0900

    settings

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,13 @@
+package com.ll.exam;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	@Test
+	public void 테스트() {
+		assertTrue(1==1);
+		assertEquals(1,1);
+	}
+}
