194152888214319752568b639abf134dc490f1be
shinyehwan <jghisjl007@naver.com>
2022-11-06 19:38:57 +0900

    WiseSayingService 도입

---
+++ src/main/java/com/ll/exam/WiseSayingRepository.java
@@ -0,0 +1,51 @@
+package com.ll.exam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WiseSayingRepository {
+	private int wiseSayingLastId;
+	private List<WiseSaying> wiseSayings;
+
+	public WiseSayingRepository() {
+		wiseSayingLastId = 0;
+		wiseSayings = new ArrayList<>();
+	}
+
+	public WiseSaying write(String content, String author) {
+		int id = ++wiseSayingLastId;
+		WiseSaying wiseSaying = new WiseSaying(id, content, author);
+		wiseSayings.add(wiseSaying);
+		System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+
+		return wiseSaying;
+	}
+
+	public List<WiseSaying> findAll() {
+		return wiseSayings;
+	}
+
+	public boolean remove(int id) {
+		WiseSaying wiseSaying = findById(id);
+		if (wiseSaying == null) {
+			return false;
+		}
+		wiseSayings.remove(wiseSaying);
+		return true;
+	}
+
+	public WiseSaying findById(int id) {
+		for (WiseSaying wiseSaying : wiseSayings) {
+			if (wiseSaying.id == id) {
+				return wiseSaying;
+			}
+		}
+		return null;
+	}
+
+	public void modify(int id, String content, String author) {
+		WiseSaying wiseSaying = findById(id);
+		wiseSaying.content = content;
+		wiseSaying.author = author;
+	}
+}
--- src/main/java/com/ll/exam/WiseSayingService.java
+++ src/main/java/com/ll/exam/WiseSayingService.java
@@ -5,48 +5,28 @@ import java.util.List;
 
 public class WiseSayingService {
 
-	private int wiseSayingLastId;
-	private List<WiseSaying> wiseSayings;
+
+	private WiseSayingRepository wiseSayingRepository;
 
 	public WiseSayingService() {
-		wiseSayingLastId = 0;
-		wiseSayings = new ArrayList<>();
+		wiseSayingRepository = new WiseSayingRepository();
 	}
 	public WiseSaying write(String content, String author) {
-		int id = ++wiseSayingLastId;
-		WiseSaying wiseSaying = new WiseSaying(id, content, author);
-		wiseSayings.add(wiseSaying);
-		System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-
-		return wiseSaying;
+		return wiseSayingRepository.write(content, author);
 	}
 
 	public List<WiseSaying> findAll() {
-		return wiseSayings;
+		return wiseSayingRepository.findAll();
 	}
 
 	public boolean remove(int id) {
-		WiseSaying wiseSaying = findById(id);
-		if (wiseSaying == null) {
-			return false;
-		}
-		wiseSayings.remove(wiseSaying);
-		return true;
+		return wiseSayingRepository.remove(id);
 	}
 	public WiseSaying findById(int id) {
-		for (WiseSaying wiseSaying : wiseSayings) {
-			if (wiseSaying.id == id) {
-				return wiseSaying;
-			}
-		}
-		return null;
+		return wiseSayingRepository.findById(id);
 	}
 
-
-
 	public void modify(int id, String content, String author) {
-		WiseSaying wiseSaying = findById(id);
-		wiseSaying.content = content;
-		wiseSaying.author = author;
+		wiseSayingRepository.modify(id, content, author);
 	}
 }
